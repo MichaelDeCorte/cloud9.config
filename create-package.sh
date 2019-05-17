3886d506733d4f970b7b8b91385ad059080529e9
Michael DeCorte <mdecorte.decorte+github@gmail.com>
2019-05-17 13:55:55 +0000

    initial checkin

---
+++ create-package.sh
@@ -0,0 +1,12 @@
+#!/bin/sh                                                                                                                                    
+
+git clone git@github.com:MichaelDeCorte/cloud9.config.git
+
+cp -rf ~/.aws/* cloud9.config/aws-config/
+
+cp -rf ~/.ssh/*.pem cloud9.config/ssh-config/
+
+zip -r cloud9.config.zip cloud9.config
+
+
+
