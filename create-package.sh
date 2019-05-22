13a7d1fd33c5b729a57ffc240f1581bc2809948e
Michael DeCorte <mdecorte.decorte+github@gmail.com>
2019-05-22 14:47:48 -0400

    bug fixes

--- create-package.sh
+++ create-package.sh
@@ -5,6 +5,7 @@ git clone git@github.com:MichaelDeCorte/cloud9.config.git
 cp -rf ~/.aws/* cloud9.config/aws-config/
 
 cp -rf ~/.ssh/*.pem cloud9.config/ssh-config/
+cp -rf ~/.ssh/*.key cloud9.config/ssh-config/
 
 zip -r cloud9.config.zip cloud9.config
 
